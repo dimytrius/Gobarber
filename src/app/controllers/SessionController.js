1447e97d47ea94cb2f0d61411daf2f36c8bcf3d1
Dimytrius <dimy.rocha@gmail.com>
2019-04-28 16:20:10 -0300

    migration created

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,14 +11,24 @@ class SessionController {
     const user = await User.findOne({ where: { email } })
 
     if (!user) {
-      console.log('usuario não encontrado')
+      req.flash('error', 'Usuário não encontrado')
       return res.redirect('/')
     }
     if (!(await user.checkPassword(password))) {
-      console.log('senha não esta correta')
+      req.flash('error', 'Senha não esta correta')
+
       return res.redirect('/')
     }
+
+    req.session.user = user
     return res.redirect('/app/dashboard')
+  }
+
+  destroy (req, res) {
+    req.session.destroy(() => {
+      res.clearCookie('root ')
+      return res.redirect('/')
+    })
   }
 }
 
